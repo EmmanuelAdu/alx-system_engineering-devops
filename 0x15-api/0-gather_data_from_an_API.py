3fd37a7b692b92a19598b36d3fb5a37300145b9e
EmmanuelAdu <christorenzo20@gmail.com>
2024-01-05 16:05:57 +0000

    Gathering data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-""" This script gathers data from an API
-"""
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
 import sys
 
 
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com/users"
-    url = base_url + "/" + employee_id
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
     response = requests.get(url)
-    employee_name = response.json().get('name')
+    employeeName = response.json().get('name')
 
-    todo_url = url + "/todos"
-    response = requests.get(todo_url)
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
     tasks = response.json()
     done = 0
     done_tasks = []
@@ -26,7 +25,7 @@ if __name__ == "__main__":
             done += 1
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, done, len(tasks)))
+          .format(employeeName, done, len(tasks)))
 
     for task in done_tasks:
-        print("     {}".format(task.get('title')))
+        print("\t {}".format(task.get('title')))
